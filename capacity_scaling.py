979dca1ad62a8805257bba2fc010c249808c10ad
rizaozcelik <rizaozcelik96@gmail.com>
2018-11-30 16:22:39 +0300

    bug fix for intermediate save

--- capacity_scaling.py
+++ capacity_scaling.py
@@ -289,14 +289,14 @@ def run_experiments(configs, solvers, solver_params, solver_names,
     if not filename:
         # If a filename is no specified, generate a random string for filename
         filename = uuid.uuid4() + '.csv'
-    write_file = open(filename, 'w')
+    with open(filename, 'w') as write_file:
+        col_names = ['iter','name', 'node_count','density', 'distribution_name', 
+                'mean', 'std', 'skewness', 'execution_time', 'Deltas', 'deltas']
+        write_file.write('|'.join(col_names) + '\n')
+
     # A flag that will be used to stop experimenting when mismatched results are 
     # found
     mismatched_result = False
-#    desired_statistics = []
-    col_names = ['iter','name', 'node_count','density', 'distribution_name', 'mean',
-                 'std', 'skewness', 'execution_time', 'Deltas', 'deltas']
-    write_file.write('|'.join(col_names) + '\n')
     # Each config has a format (node count, density, stats) where stats in another
     # tuple that contains distrubution name, mean, std, skewness.
     iter_count = -1
@@ -330,14 +330,15 @@ def run_experiments(configs, solvers, solver_params, solver_names,
                                           'ERROR', 'ERROR', 'ERROR']
                 if iter_count % save_frequency == 0:
                     desired_statistics = [str(stat) for stat in desired_statistics]
+                    write_file = open(filename, 'a')
                     write_file.write('|'.join(desired_statistics) + '\n')
+                    write_file.close()
             if len(set(results)) != 1:
                 mismatched_result = True
                 print('Wrong result')
         else:
             break
 
-    write_file.close()
 
 #%%
 statistical_params = [('normal',1000,50,-10),
